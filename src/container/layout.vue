a8162af6993508112686b08ad90aff99e2469b26
wangyupo <wangyupo@aiwith.com>
2018-07-11 15:32:36 +0800

    Initial commit

---
+++ src/container/layout.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container">
+        <header>this is header</header>
+        <div class="content">this is container</div>
+        <footer class="footer">this is footer</footer>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Layout"
+    }
+</script>
+
+<style lang="scss" scoped>
+    .container {
+
+    }
+</style>
---
+++ src/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <Layout></Layout>
+</template>
+
+<script>
+    import Layout from '@/container/layout';
+    export default {
+        name: "",
+        components: {
+            Layout
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
