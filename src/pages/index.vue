6fcac80b0e4659f27c119d33530770f22eeaa032
wangyupo <wangyupo@aiwith.com>
2018-07-11 15:50:18 +0800

    常规提交

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,15 +1,21 @@
 <template>
     <Layout>
-        {{$t('message.hello')}}
+        <p @click="switchLang">{{$t('message.hello')}} <- 点这里切换语言</p>
     </Layout>
 </template>
 
 <script>
     import Layout from '@/container/layout';
+
     export default {
         name: "",
         components: {
             Layout
+        },
+        methods: {
+            switchLang() {
+                this.$i18n.locale = this.$i18n.locale === 'cn' ? 'en' : 'cn';
+            }
         }
     }
 </script>
