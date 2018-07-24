664d6fefd511c6f320a47f7c326c8b1fb09cb863
wangyupo <wangyupo@aiwith.com>
2018-07-24 11:32:15 +0800

    router添加按需加载

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,11 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+
+// 同步加载
 import Home from '@/pages/index';
+
+// 按需加载
+// const Home = () => import('@/pages/index');
 
 Vue.use(Router);
 
