899ac3f3e35feabea2bc2ed8b165c0719d6840e3
wangyupo <wangyupo@aiwith.com>
2018-07-25 10:59:37 +0800

    添加utils

--- src/config/utils.js
+++ src/config/utils.js
@@ -24,3 +24,13 @@ export const removeStore = name => {
     if (!name) return;
     window.localStorage.removeItem(name);
 };
+
+/**
+ * 格式化手机号为152****8888
+ */
+export const formatPhone = (phone) => {
+    if (typeof phone === 'number') {
+        phone = phone.toString();
+    }
+    return phone.substr(0, 3) + '****' + phone.substr(7, 11);
+};
